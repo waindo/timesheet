7290695f6b98b9c1c4065d8d3fc96d15c163ff2e
Nofendra Wahyudi <wahyudi.d.fang@gmail.com>
2014-05-05 11:08:12 +0700

    Tabel Project Log

---
+++ app/database/migrations/2014_05_05_030925_t_PRJCT_LOGGS.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class TPRJCTLOGGS extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::create('t_PRJCT_LOGGS', function($table){
+         $table->string('prjctParam',1);
+         $table->string('prjctIdent',50);
+         $table->Integer('prjctUsrid',50);
+         $table->string('prjctNames',100);
+         $table->tinyInteger('prjctInsta');
+		 $table->string('prjctInsta',4);
+         $table->timestamps();
+      });
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::drop('t_PRJCT_LOGGS');
+	}
+
+}
