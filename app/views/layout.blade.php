6299bdf7c08c2cb7e7b15e2f5cdbde94db0fe04a
mirza annas <miruzaannas@gmail.com>
2014-05-07 10:01:54 +0700

    tambah form project

--- app/views/layout.blade.php
+++ app/views/layout.blade.php
@@ -2,7 +2,7 @@
 <html lang='en'>
 <head>
  <meta charset='utf-8' />
- <title>CRUD App</title>
+ <title>Tambah Project</title>
 
 <meta name='viewport' content='width=device-width, initial-scale=1, maximum-scale=1' />
  {{ HTML::style('css/bootstrap.min.css') }}
@@ -14,6 +14,16 @@
 
 {{ HTML::script('js/jquery.min.js') }}
 {{ HTML::script('js/bootstrap.js') }}
+
+ <link rel="stylesheet" href="http://code.jquery.com/ui/1.9.2/themes/base/jquery-ui.css" />
+    <script src="http://code.jquery.com/jquery-1.8.3.js"></script>
+    <script src="http://code.jquery.com/ui/1.9.2/jquery-ui.js"></script>
+    <link rel="stylesheet" href="/resources/demos/style.css" />
+    <script>
+    $(function() {
+        $( "#datepicker" ).datepicker();
+    });
+    </script>
 
 </body>
 </html>
